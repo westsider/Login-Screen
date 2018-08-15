e5ce7bb8f448c5d57cc5819bcc30d770cb9b14a7
Warren Hansen <whansen1@me.com>
2018-08-15 15:07:41 -0500

    task: finished textfield annimation

--- Login Screen/ViewController.swift
+++ Login Screen/ViewController.swift
@@ -27,10 +27,24 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var emailHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var passwordHeightConstraing: NSLayoutConstraint!
+    
+    @IBOutlet weak var mainStackViewHeight: NSLayoutConstraint!
+    
     let textFieldParser = TextFieldParser()
+    
+    var showFieldsState:Bool = true
+    
+    var start:CGFloat = 300
+    var finish:CGFloat = 330
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailHeightConstraint.constant = 0
+        passwordHeightConstraing.constant = 0
+        mainStackViewHeight.constant = start
     }
     
     @IBAction func createAccountButtonTapped(_ sender: UIButton) {
@@ -40,4 +54,33 @@ class LoginViewController: UIViewController {
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         textFieldParser.checkInputAndLogin(signUp: false, user: User(email:  emailTextField.text!, password: passwordTextField.text!))
     }
+    
+    @IBAction func expandTextfieldAction(_ sender: UIButton) {
+        showAndHideTextFields(now: showFieldsState)
+        if showFieldsState { showFieldsState = false } else { showFieldsState = true }
+    }
+    
+    func showAndHideTextFields(now:Bool) {
+        
+        if now {
+            emailHeightConstraint.constant = 30
+            passwordHeightConstraing.constant = 30
+            mainStackViewHeight.constant = finish
+        } else {
+            emailHeightConstraint.constant = 0
+            passwordHeightConstraing.constant = 0
+            mainStackViewHeight.constant = start
+        }
+        
+        UIView.animate(
+            withDuration: 0.6,
+            delay: 0.0,
+            usingSpringWithDamping: 0.5,
+            initialSpringVelocity: 0.01,
+            options: [],
+            animations: {
+                self.view.layoutIfNeeded()
+        })
+    }
+    
 }
